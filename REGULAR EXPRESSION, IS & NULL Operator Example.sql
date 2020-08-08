5f95fdf4348525ca5df306394656e4837f8e51a4
Niraj Raju Lakhadive <68545482+nirajlakhadive@users.noreply.github.com>
2020-08-09 04:57:18 +0530

    REGULAR EXPRESSION, IS & NULL Operator Example.sql
    This query is an example of how to use REGULAR EXPRESSION, IS & NULL Operator

---
+++ REGULAR EXPRESSION, IS & NULL Operator Example.sql
@@ -0,0 +1,11 @@
+SELECT *
+FROM customers 
+WHERE first_name REGEXP 'ELKA|AMBUR'
+
+-- WHERE last_name REGEXP 'EY$|ON$'
+-- WHERE last_name REGEXP '^MY|SE'
+-- WHERE last_name REGEXP 'b[ru]'
+
+-- following code is FROM orders table
+-- WHERE shipped_date IS  NULL
+
